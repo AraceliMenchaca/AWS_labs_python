0d718873021e1fb527279acb5a81a293032ac029
Araceli Menchaca <araceli.menchaca.sanchez@gmail.com>
2023-12-01 11:05:51 +0100

    Se creo el archivo lab_111 con listas, tuplas y diccionarios

---
+++ lab_111.py
@@ -0,0 +1,49 @@
+# Laboratorio 111 - Lista, Tupla, Diccionario
+
+# EJERCICIO 1 - List
+fruitList = ['apple', 'banana', 'cherry']
+print(fruitList)
+print(type(fruitList))
+
+# Accessing a list position
+#apple
+print(fruitList[0])
+#banana
+print(fruitList[1])
+#cherry
+print(fruitList[2])
+
+# Changing values in a list
+fruitList[2] = 'orange'
+print(fruitList)
+
+# EJERCICIO 2 - Tuple
+fruitTuple = ('apple', 'banana', 'pineapple')
+print(fruitTuple)
+print(type(fruitTuple))
+
+# Accessing a tuple position
+#apple
+print(fruitTuple[0])
+#banana
+print(fruitTuple[1])
+#cherry
+print(fruitTuple[2])
+
+# EJERCICIO 3 - Dictionaries
+fruitDictionary = {
+    'Akua' : "apple",
+    "Saanvi" : "banana",
+    "Paulo" : "pinapple"
+}
+print(fruitDictionary)
+print(type(fruitDictionary))
+
+# Accesing a dictionary position
+#Akua
+print(fruitDictionary['Akua'])
+#Saavi
+print(fruitDictionary['Saanvi'])
+#Paulo
+print(fruitDictionary['Paulo'])
+
